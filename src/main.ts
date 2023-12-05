8a3a188b30fdbcae695e2429321d42534f1e883a
1552980358 <1552980358@qq.com>
2023-12-05 12:13:11 +0800

    main.ts: Update setup procedure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 
-const vue = createApp(App)
-vue.mount('#app')
+import router from '@/router/router'
+import i18n, {i18nLoadText} from '@/i18n/i18n'
+import vuetify from '@/vuetify/vuetify'
+import axiosSetup from '@/axios/axios-setup'
+import 'material-symbols'
+
+router.beforeEach(async () => {
+    await i18nLoadText()
+})
+
+axiosSetup()
+
+createApp(App)
+    .use(router)
+    .use(i18n)
+    .use(vuetify)
+    .mount('#app')

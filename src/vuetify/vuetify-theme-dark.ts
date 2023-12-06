2999c66a2a9ed1437a291386d01145f2e67b26a8
1552980358 <1552980358@qq.com>
2023-12-07 03:16:30 +0800

    Vuetify: Update colors to light and dark themes

--- src/vuetify/vuetify-theme-dark.ts
+++ src/vuetify/vuetify-theme-dark.ts
@@ -1,22 +1,29 @@
-import {ThemeDefinition, useTheme} from 'vuetify'
+import {ThemeDefinition} from 'vuetify'
 
 const dark: ThemeDefinition = {
     dark: true,
     colors: {
-        primary: '#ABC7FF',
+        'primary': '#ABC7FF',
         'on-primary': '#002F66',
+        'primary-container': '#05458E',
+        'on-primary-container': '#d7e2ff',
 
-        secondary: '#BEC6dC',
+        'secondary': '#BEC6dC',
         'on-secondary': '#283041',
+        'secondary-container': '#3E4759',
+        'on-secondary-container': '#DAE2f9',
 
-        info: '#DDBCE0',
+        'info': '#DDBCE0',
         'on-info': '#3F2844',
+        'info-container': '#573E5C',
+        'on-info-container': '#FAD8FD',
 
-        background: '#1A1B1F',
+        'background': '#1A1B1F',
         'on-background': '#E3E2E6',
 
-        surface: '#121316',
+        'surface': '#121316',
         'on-surface': '#C7C6CA',
+        'surface-container': '#1F1F23',
 
         // success: string,
         // 'on-success': string,
@@ -24,8 +31,10 @@ const dark: ThemeDefinition = {
         // warning: string,
         // 'on-warning': string,
 
-        error: '#FFB4AB',
+        'error': '#FFB4AB',
         'on-error': '#690005',
+        'error-container': '#93000A',
+        'on-error-container': '#FFDAD6',
     }
 }
 
--- src/vuetify/vuetify-theme-light.ts
+++ src/vuetify/vuetify-theme-light.ts
@@ -1,22 +1,29 @@
-import {ThemeDefinition, useTheme} from 'vuetify'
+import {ThemeDefinition} from 'vuetify'
 
 const light: ThemeDefinition = {
     dark: false,
     colors: {
-        primary: '#2D5DA7',
+        'primary': '#2D5DA7',
         'on-primary': '#FFF',
+        'primary-container': '#D7E2FF',
+        'on-primary-container': '#001B3F',
 
-        secondary: '#565E71',
+        'secondary': '#565E71',
         'on-secondary': '#FFF',
+        'secondary-container': '#DAE2F9',
+        'on-secondary-container': '#131c2b',
 
-        info: '#705574',
+        'info': '#705574',
         'on-info': '#FFF',
+        'info-container': '#FAd8FD',
+        'on-info-container': '#29132E',
 
-        background: '#FDFBFF',
+        'background': '#FDFBFF',
         'on-background': '#1A1B1F',
 
-        surface: '#FAf9FD',
+        'surface': '#FAf9FD',
         'on-surface': '#1A1B1F',
+        'surface-container': '#EFEDF1',
 
         // success: string,
         // 'on-success': string,
@@ -24,8 +31,10 @@ const light: ThemeDefinition = {
         // warning: string,
         // 'on-warning': string,
 
-        error: '#BA1A1A',
+        'error': '#BA1A1A',
         'on-error': '#FFF',
+        'error-container': '#ffDAD6',
+        'on-error-container': '#410002',
     }
 }
 

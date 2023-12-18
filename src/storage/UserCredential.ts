ebbfe2996a18af3b799dffb7a475a2e20d4bf85c
1552980358 <1552980358@qq.com>
2023-12-19 01:34:55 +0800

    LocalStorage: Update UserCredential item

---
+++ src/storage/UserCredential.ts
@@ -0,0 +1,40 @@
+import {readStorage, removeStorage, writeStorage} from '@/storage/LocalStorage'
+
+export default class UserCredential {
+
+    private static readonly KEY = 'UserCredential'
+
+    static read(): UserCredential {
+        const rawData = readStorage(this.KEY)
+        if (!!rawData) {
+            try {
+                return JSON.parse(atob(rawData))
+            } catch (e) {
+                // Remove wrong data
+                this.remove()
+            }
+        }
+        return null
+    }
+
+    static write(jwt: string, username: string, password: string) {
+        const userCredential = new UserCredential(jwt, username, password)
+        const base64 = btoa(JSON.stringify(userCredential))
+        writeStorage(this.KEY, base64)
+    }
+
+    private static remove() {
+        removeStorage(this.KEY)
+    }
+
+    jwt: string
+    username: string
+    password: string
+
+    constructor(jwt: string, username: string, password: string) {
+        this.jwt = jwt
+        this.username = username
+        this.password = password
+    }
+
+}

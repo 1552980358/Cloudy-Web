c634037a38406971a9bb2dd35e504b9410ca8168
1552980358 <1552980358@qq.com>
2023-12-19 18:33:22 +0800

    Axios: Update AxiosAuthorization

--- src/axios/axios-authorization.ts
+++ src/axios/axios-authorization.ts
@@ -1,7 +1,30 @@
 import axios from 'axios'
+import UserCredential from '@/storage/UserCredential'
 
-const axiosAuthorization = (jwt: string) => {
-    axios.defaults.headers.common['Authorization'] = `JWT ${jwt}`
+class AxiosAuthorization {
+
+    private static readonly Authorization = 'Authorization'
+
+    static setCredential(userCredential: UserCredential) {
+        this.setJWT(userCredential.jwt)
+    }
+
+    static setJWT(jwt: string) {
+        axios.defaults.headers.common[this.Authorization] = this.jwtHeader(jwt)
+    }
+
+    static headerConfig(userCredential: UserCredential): { headers: { Authorization: string } } {
+        return { headers: { Authorization: this.jwtHeader(userCredential.jwt) } }
+    }
+
+    private static jwtHeader(jwt: string): string {
+        return `JWT ${jwt}`
+    }
+
+    static clear() {
+        delete axios.defaults.headers.common[this.Authorization]
+    }
+
 }
 
-export default axiosAuthorization;
+export default AxiosAuthorization

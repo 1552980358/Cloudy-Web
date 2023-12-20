039ac7767944833bd6b176c523c1eefdccf058ea
1552980358 <1552980358@qq.com>
2023-12-20 18:52:02 +0800

    Axios: Rename `headerConfig` as `authorization` (AxiosAuthorization)

--- src/axios/axios-authorization.ts
+++ src/axios/axios-authorization.ts
@@ -5,14 +5,10 @@ class AxiosAuthorization {
     private static readonly Authorization = 'Authorization'
 
     static setToken(jwt: string) {
-        axios.defaults.headers.common[this.Authorization] = this.headerContent(jwt)
+        axios.defaults.headers.common[this.Authorization] = this.authorization(jwt)
     }
 
-    static headerConfig(userCredential: UserCredential): { headers: { Authorization: string } } {
-        return { headers: { Authorization: this.jwtHeader(userCredential.jwt) } }
-    }
-
-    private static jwtHeader(jwt: string): string {
+    private static authorization(jwt: string): string {
         return `JWT ${jwt}`
     }
 

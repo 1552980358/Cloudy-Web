6795d9ba1b1ceb5d62ccb68432e095b018d7a72f
1552980358 <1552980358@qq.com>
2023-12-20 18:51:13 +0800

    Axios: Rename `setJWT` as `setToken` (AxiosAuthorization)

--- src/axios/axios-authorization.ts
+++ src/axios/axios-authorization.ts
@@ -1,16 +1,11 @@
 import axios from 'axios'
-import UserCredential from '@/storage/user-credential'
 
 class AxiosAuthorization {
 
     private static readonly Authorization = 'Authorization'
 
-    static setCredential(userCredential: UserCredential) {
-        this.setJWT(userCredential.jwt)
-    }
-
-    static setJWT(jwt: string) {
-        axios.defaults.headers.common[this.Authorization] = this.jwtHeader(jwt)
+    static setToken(jwt: string) {
+        axios.defaults.headers.common[this.Authorization] = this.headerContent(jwt)
     }
 
     static headerConfig(userCredential: UserCredential): { headers: { Authorization: string } } {

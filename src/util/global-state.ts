6fafd79387e62309bd8295b46fb0853989e9c626
1552980358 <1552980358@qq.com>
2023-12-30 01:57:11 +0800

    util: Update account state (Global-State)

--- src/util/global-state.ts
+++ src/util/global-state.ts
@@ -3,3 +3,7 @@ import {InjectionKey} from 'vue'
 export const Authorization: InjectionKey<{
     isCompleted: boolean, isAuthorized: boolean
 }> = Symbol()
+
+export const Account: InjectionKey<{
+    id: string | null, username: string | null, nickname: string | null
+}> = Symbol()

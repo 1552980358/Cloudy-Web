a17c7752bdf9009ae013817de70ad526780aa92b
1552980358 <1552980358@qq.com>
2024-02-01 19:48:21 +0800

    assets: Update text.css

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import VueI18n, { setupVueI18n } from '@/vue-i18n/vue-i18n.ts';
 import '@/material-symbols/material-symbols.ts';
 
 import '@/assets/css/index.css';
+import '@/assets/css/text.css';
 
 // Setup VueI18n before navigation
 VueRouter.beforeEach(setupVueI18n);
